9a90573eb61a79f9f65862910c7ea8bd9fd87c4d
Styve Simonneau <styve.simonneau@gmail.com>
2015-05-10 20:25:40 +0200

    Command parser optionnal args

--- ClientTinyTank/src/main/java/com/lefrantguillaume/Utils/configs/MasterConfig.java
+++ ClientTinyTank/src/main/java/com/lefrantguillaume/Utils/configs/MasterConfig.java
@@ -6,7 +6,7 @@ package com.lefrantguillaume.Utils.configs;
 public class MasterConfig {
     private static final int masterUdpPort = 13444;
     private static final int masterTcpPort = 13333;
-    private static final String masterAddress = "localhost";
+    private static final String masterAddress = "109.22.252.229";
     private static final String mapConfigFile = "mapConfig";
 
     public MasterConfig() {
