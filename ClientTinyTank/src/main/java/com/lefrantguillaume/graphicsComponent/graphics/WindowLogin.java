6585e0e67d70f72e70a4b573e4fb073cf3510079
Styve Simonneau <styve.simonneau@gmail.com>
2015-05-30 14:31:58 +0200

    fonctionnel

--- ClientTinyTank/src/main/java/com/lefrantguillaume/graphicsComponent/graphics/WindowLogin.java
+++ ClientTinyTank/src/main/java/com/lefrantguillaume/graphicsComponent/graphics/WindowLogin.java
@@ -1,152 +1,57 @@
 package com.lefrantguillaume.graphicsComponent.graphics;
 
-import com.lefrantguillaume.Utils.tools.StringTools;
-import com.lefrantguillaume.graphicsComponent.input.InputCheck;
+import com.lefrantguillaume.Utils.configs.CurrentUser;
+import com.lefrantguillaume.Utils.stockage.Pair;
+import com.lefrantguillaume.Utils.tools.Debug;
+import com.lefrantguillaume.networkComponent.networkData.DataServer;
 import de.lessvoid.nifty.Nifty;
-import de.lessvoid.nifty.lwjglslick.input.LwjglInputSystem;
-import de.lessvoid.nifty.nulldevice.NullInputSystem;
 import de.lessvoid.nifty.nulldevice.NullSoundDevice;
+import de.lessvoid.nifty.renderer.lwjgl.input.LwjglInputSystem;
 import de.lessvoid.nifty.renderer.lwjgl.render.LwjglRenderDevice;
-import de.lessvoid.nifty.spi.input.InputSystem;
+import de.lessvoid.nifty.slick2d.input.PlainSlickInputSystem;
+import de.lessvoid.nifty.slick2d.input.SlickInputSystem;
 import de.lessvoid.nifty.spi.time.impl.AccurateTimeProvider;
 import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.opengl.SlickCallable;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.util.UUID;
+
 
 /**
  * Created by andres_k on 10/03/2015.
  */
-/*
-public class WindowLogin extends BasicGameState {
-    private GameContainer container;
-    private StateBasedGame stateGame;
-    private InputCheck input;
-    private int id;
-    private TextField fLogin = null;
-    private TextField fPassword = null;
-    private Image logo = null;
-    private Font font = null;
-
-
-    public WindowLogin(int id) throws JSONException {
-        this.id = id;
-        this.input = new InputCheck(StringTools.readFile("configInput.json"));
-
-    }
-
-    @Override
-    public int getID() {
-        return this.id;
-    }
-
-    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        this.container = gameContainer;
-        this.stateGame = stateBasedGame;
-        this.container.setForceExit(false);
-        this.logo = new Image("assets/img/logo.png");
-        this.font = new TrueTypeFont(new java.awt.Font(java.awt.Font.SANS_SERIF, java.awt.Font.PLAIN, 26), false);
-        this.fLogin = new TextField(gameContainer, this.font, 320, 300, 640, 40);
-        this.fPassword = new TextField(gameContainer, this.font, 320, 380, 640, 40);
-    }
-
-    @Override
-    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        this.container.setTargetFrameRate(10);
-        this.container.setShowFPS(false);
-        this.container.setAlwaysRender(false);
-        this.container.setVSync(false);
-    }
-
-    @Override
-    public void leave(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-
-    }
-
-    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics g) throws SlickException {
-        g.setBackground(org.newdawn.slick.Color.white);
-        this.logo.draw(320, 100, 0.5f);
-        this.fLogin.render(gameContainer, g);
-        this.fPassword.render(gameContainer, g);
-    }
-
-    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-
-    }
-
-    @Override
-    public void keyPressed(int key, char c) {
-    }
-
-    @Override
-    public void keyReleased(int key, char c) {
-        if (key == Input.KEY_RETURN) {
-            if (this.container.getInput().isKeyDown(Input.KEY_LCONTROL) || this.container.getInput().isKeyDown(Input.KEY_RCONTROL)) {
-
-                // TODO REMOVE THIS ON RELEASE
-                CurrentUser.setId(UUID.randomUUID().toString());
-                CurrentUser.setPseudo(this.fLogin.getText());
-                this.stateGame.enterState(EnumWindow.ACCOUNT.getValue());
-
-            } else {
-                try {
-                    Pair<Boolean, String> p = DataServer.authentification(this.fLogin.getText(), this.fPassword.getText());
-
-                    if (p.getV1()) {
-
-                        JSONObject object = new JSONObject(p.getV2());
-
-                        Debug.debug("my id = " + object.get("_id"));
-
-                        CurrentUser.setId(object.get("_id").toString());
-                        CurrentUser.setPseudo(object.get("username").toString());
-                        this.stateGame.enterState(EnumWindow.ACCOUNT.getValue());
-                    } else {
-
-                        // TODO Display message on client.
-                        Debug.debug("Error Login : " + p.getV2());
-
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else if (key == Input.KEY_TAB) {
-            if (this.fLogin.hasFocus())
-                this.fPassword.setFocus(true);
-            else if (this.fPassword.hasFocus()) {
-                this.fLogin.setFocus(true);
-            }
-        } else if (key == Input.KEY_ESCAPE) {
-            this.container.exit();
-        }
-    }
-
-}
-*/
-
 
 class WindowLogin extends BasicGameState {
-    private InputCheck input;
+    private GameContainer container;
+    private StateBasedGame stateGame;
+    private LwjglInputSystem inputSys;
     private int id;
 
     private Nifty nifty;
 
     public WindowLogin(int id) throws JSONException {
         this.id = id;
-        this.input = new InputCheck(StringTools.readFile("configInput.json"));
-
+        //this.input = new InputCheck(StringTools.readFile("configInput.json"));
     }
 
-    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+        this.container = gameContainer;
+        this.stateGame = stateBasedGame;
+        this.container.setForceExit(false);
         try {
-            InputSystem input = new LwjglInputSystem();
-            ((LwjglInputSystem)input).startup();
-            nifty = new Nifty(new LwjglRenderDevice(), new NullSoundDevice(), new NullInputSystem(), new AccurateTimeProvider());
+            SlickInputSystem inputSys = new PlainSlickInputSystem();
+            nifty = new Nifty(new LwjglRenderDevice(), new NullSoundDevice(), inputSys, new AccurateTimeProvider());
+
+            gameContainer.getInput().removeListener(this);
+            gameContainer.getInput().removeListener(inputSys);
+            gameContainer.getInput().addListener(inputSys);
 
             nifty.loadStyleFile("nifty-default-styles.xml");
             nifty.loadControlFile("nifty-default-controls.xml");
@@ -172,5 +77,60 @@ class WindowLogin extends BasicGameState {
     @Override
     public int getID() {
         return this.id;
+    }
+
+    @Override
+    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+        this.container.setTargetFrameRate(60);
+        this.container.setShowFPS(false);
+        this.container.setAlwaysRender(false);
+        this.container.setVSync(false);
+    }
+
+    @Override
+    public void keyPressed(int key, char c) {
+        System.out.println("key pressed:" + c);
+    }
+
+    @Override
+    public void keyReleased(int key, char c) {
+        if (key == Input.KEY_RETURN) {
+            if (this.container.getInput().isKeyDown(Input.KEY_LCONTROL) || this.container.getInput().isKeyDown(Input.KEY_RCONTROL)) {
+
+                // TODO REMOVE THIS ON RELEASE
+                CurrentUser.setId(UUID.randomUUID().toString());
+                CurrentUser.setPseudo("login"); //TODO remplacer login par la vraie value du loginText
+                this.stateGame.enterState(EnumWindow.ACCOUNT.getValue());
+
+            } else {
+                try {
+                    Pair<Boolean, String> p = DataServer.authentification("user", "pass"); // TODO voir le TODO d'au dessus
+
+                    if (p.getV1()) {
+
+                        JSONObject object = new JSONObject(p.getV2());
+
+                        Debug.debug("my id = " + object.get("_id"));
+
+                        CurrentUser.setId(object.get("_id").toString());
+                        CurrentUser.setPseudo(object.get("username").toString());
+                        this.stateGame.enterState(EnumWindow.ACCOUNT.getValue());
+                    } else {
+
+                        // TODO Display message on client.
+                        Debug.debug("Error Login : " + p.getV2());
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (key == Input.KEY_ESCAPE) {
+            this.container.exit();
+        }
+    }
+
+    public void connect() {
+        System.out.println("Cliqué sur Connect !");
     }
 }
--- ClientTinyTank/src/main/java/com/lefrantguillaume/graphicsComponent/graphics/controllers/LoginController.java
+++ ClientTinyTank/src/main/java/com/lefrantguillaume/graphicsComponent/graphics/controllers/LoginController.java
@@ -1,17 +1,30 @@
 package com.lefrantguillaume.graphicsComponent.graphics.controllers;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyEventSubscriber;
+import de.lessvoid.nifty.controls.TextField;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.elements.events.NiftyMousePrimaryClickedEvent;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.screen.KeyInputHandler;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
 /**
  * Created by Styve on 27/05/2015.
  */
-public class LoginController implements ScreenController{
+public class LoginController implements ScreenController, KeyInputHandler {
+    private Nifty nifty;
+    private Screen screen;
+    private Element loginInput;
+
     @Override
     public void bind(Nifty nifty, Screen screen) {
         // on est bind dans le xml. le screen id="start" est relié à ce controlleur
         System.out.println("LoginController binded");
+        this.nifty = nifty;
+        this.screen = screen;
+        this.loginInput = screen.findElementByName("login");
     }
 
     @Override
@@ -23,6 +36,15 @@ public class LoginController implements ScreenController{
     public void onEndScreen() {
         System.out.println("LoginController ended");
     }
+
+    @Override
+    public boolean keyEvent(NiftyInputEvent e) {
+        return false;
+    }
+
+    @NiftyEventSubscriber(id="ConnectButton")
+    public void onClick(String id, NiftyMousePrimaryClickedEvent event) {
+        System.out.println("element with id [" + id + "] clicked at [" + event.getMouseX() + ", " + event.getMouseY() + "]"); }
 
     public void connect() {
         System.out.println("Cliqué sur Connect !");
