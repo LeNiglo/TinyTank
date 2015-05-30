ba514b362c2dc79ca14f3d10479ab667daeb5a69
LeNiglo <lefrantguillaume@gmail.com>
2015-05-30 05:43:03 +0200

    build sh updated

--- build.sh
+++ build.sh
@@ -1,33 +1,52 @@
+function web {
+
+  echo "Building Web"
+  cd WebTinyTank
+  sudo meteor update
+  sudo meteor build .
+  sudo mv WebTinyTank.tar.gz /opt/tinytank/web/.
+  cd /opt/tinytank/web/
+  sudo service tinytankweb stop
+  sudo rm -rf bundle/
+  sudo tar -zxf WebTinyTank.tar.gz
+  cd bundle/programs/server/
+  sudo npm install
+  cd /opt/tinytank
+  sudo chown tinytank -R /opt/tinytank
+  sudo service tinytankweb start
+
+}
+
+function data {
+
+  echo "Building Data"
+  sudo tar -zcf DataTinyTank.tar.gz DataTinyTank
+  sudo mv DataTinyTank.tar.gz /opt/tinytank/.
+  cd /opt/tinytank
+  sudo service tinytankdata stop
+  sudo rm -rf data
+  sudo tar -zxf DataTinyTank.tar.gz
+  sudo mv DataTinyTank data
+  cd data
+  sudo npm install
+  cd /opt/tinytank
+  sudo chown tinytank -R /opt/tinytank
+  sudo service tinytankdata start
+
+}
+
+if [ $# -eq 0 ]; then
+  web
+  data
+else
+  $1
+fi
+
 git pull
 current=`pwd`
-echo "Building Web"
-cd WebTinyTank
-sudo meteor update
-sudo meteor build .
-sudo mv WebTinyTank.tar.gz /opt/tinytank/web/.
-cd /opt/tinytank/web/
-sudo service tinytankweb stop
-sudo rm -rf bundle/
-sudo tar -zxf WebTinyTank.tar.gz
-cd bundle/programs/server/
-sudo npm install
-cd /opt/tinytank
-sudo chown tinytank -R /opt/tinytank
-sudo service tinytankweb start
+
 cd $current
-echo "Building Data"
-sudo tar -zcf DataTinyTank.tar.gz DataTinyTank
-sudo mv DataTinyTank.tar.gz /opt/tinytank/.
-cd /opt/tinytank
-sudo service tinytankdata stop
-sudo rm -rf data
-sudo tar -zxf DataTinyTank.tar.gz
-sudo mv DataTinyTank data
-cd data
-sudo npm install
-cd /opt/tinytank
-sudo chown tinytank -R /opt/tinytank
-sudo service tinytankdata start
+
 cd $current
 sudo service nginx restart
 sudo rm -f /opt/tinytank/DataTinyTank.tar.gz /opt/tinytank/web/WebTinyTank.tar.gz
