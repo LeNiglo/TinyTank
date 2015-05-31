6629bd86d3b3a48217fa1cb430014fff5de9a439
LeNiglo@Home <lefrantguillaume@gmail.com>
2015-06-01 00:29:32 +0200

    try pictures ...

--- WebTinyTank/client/views/home/about.js
+++ WebTinyTank/client/views/home/about.js
@@ -2,16 +2,20 @@ Template.about.helpers({
     "users": function () {
         return [
             {
-                name: "Guillaume Lefrant"
+                name: "Guillaume Lefrant",
+                picture: "http://puu.sh/i7GKO.jpg"
             },
             {
-                name: "Kévin Andres"
+                name: "Kévin Andres",
+                picture: "http://puu.sh/i7GBq.jpg"
             },
             {
-                name: "Styve Simonneau"
+                name: "Styve Simonneau",
+                picture: "http://puu.sh/gsacK/ffaf9995bf.jpg"
             },
             {
-                name: "Alexandre Quintin"
+                name: "Alexandre Quintin",
+                picture: "#"
             }
         ]
     }
