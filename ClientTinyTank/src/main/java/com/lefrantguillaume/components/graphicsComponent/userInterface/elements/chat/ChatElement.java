e44f814990582aef4ed81aae1494389c36202aca
andres_k <kevin.andres@epitech.eu>
2015-07-09 02:11:43 +0200

    uu

--- ClientTinyTank/src/main/java/com/lefrantguillaume/components/graphicsComponent/userInterface/elements/chat/ChatElement.java
+++ ClientTinyTank/src/main/java/com/lefrantguillaume/components/graphicsComponent/userInterface/elements/chat/ChatElement.java
@@ -45,7 +45,7 @@ public class ChatElement extends InterfaceElement {
 
     private void childInit() {
         this.selectionField = new SelectionField(new StringElement(new BodyRect(new Rectangle(this.body.getMinX() + 20, this.body.getMinY() + 170, 300, StringTools.charSizeY()), new Color(0.2f, 0.2f, 0.3f, 0.6f)),
-                new StringTimer(""), Color.white, Element.PositionInBody.LEFT_MID), EnumOverlayElement.SELECT_SHIELD.getValue());
+                new StringTimer(""), Color.white, Element.PositionInBody.LEFT_MID), EnumOverlayElement.SELECT_SHIELD.getValue() + "chat");
         float chatSizeY = 170;
         this.stringListElement = new StringListElement(new BodyRect(new Rectangle(this.body.getMinX(), this.body.getMinY(), this.body.getSizeX(), chatSizeY)));
     }
