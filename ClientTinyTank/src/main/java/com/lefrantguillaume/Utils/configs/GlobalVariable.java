b74b30d7a93b0849d6f47668d79c7ad21c9b6adf
andres_k <kevin.andres@epitech.eu>
2015-07-29 14:08:48 +0200

    debug-

--- ClientTinyTank/src/main/java/com/lefrantguillaume/Utils/configs/GlobalVariable.java
+++ ClientTinyTank/src/main/java/com/lefrantguillaume/Utils/configs/GlobalVariable.java
@@ -4,5 +4,5 @@ package com.lefrantguillaume.utils.configs;
  * Created by andres_k on 12/07/2015.
  */
 public class GlobalVariable {
-    public static boolean debug = true;
+    public static boolean debug = false;
 }
